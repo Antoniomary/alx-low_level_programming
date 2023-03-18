fbca60cb0e20aa689eb92cfff835519a03f1b218
Antoniomary <amanso.totti@yahoo.com>
2023-03-17 17:29:42 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - It prints the small alphabet letters 'a' to 'z'.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char alp[] = "abcdefghijklmnopqrstuvwxyz";
+
+	int i;
+
+	i = 0;
+	while (i <= 26)
+	{
+		putchar(alp[i]);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
