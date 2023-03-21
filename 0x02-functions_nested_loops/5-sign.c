87f6c9665d61ff6284deed9842c7865c160be4a2
Antoniomary <amanso.totti@yahoo.com>
2023-03-21 11:37:59 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number
+ * @n: It is the decimal of an ascii code character
+ * Return: 1 if number is greater than 0 or
+ * 0 if number is zero or
+ * -1 if number is leas than zero
+ */
+int print_sign(int n)
+{
+	if (n == 47)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n > 47)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+
+	}
+	_putchar('\n');
+}
