69c6deda7c7bb5dd37b865ad1d22fba762565cba
Antoniomary <amanso.totti@yahoo.com>
2023-03-22 08:54:01 -0700

    0-main.c, main.h

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* main - tests function that prints '0 is zero' when fed 0.
+*
+* Return: 0
+*/
+
+int main(void)
+{
+        int i;
+
+        i = 0;
+        positive_or_negative(i);
+
+        return (0);
+}
