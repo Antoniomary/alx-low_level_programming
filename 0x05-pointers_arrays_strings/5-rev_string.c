c7ea5683103da0c131bad824ab878f2290b1d97f
Antoniomary <amanso.totti@yahoo.com>
2023-03-27 14:23:24 -0700

    fixed relational operator in 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * rev_string - it  reverses a string.
+ * rev_string - it reverses a string.
  * @s: the string to bs reversed.
  * Return: void
  */
@@ -12,11 +12,11 @@ void rev_string(char *s)
 	count = 0;
 	for (slen = 0; s[slen] != '\0'; slen++)
 	{
-		 count++;
+		count++;
 	}
 	m = count - 1;
 	n = m;
-	for (i = 0; i < n/2; i++)
+	for (i = 0; i <= n / 2; i++)
 	{
 		temp = s[i];
 		s[i] = s[n - i];
