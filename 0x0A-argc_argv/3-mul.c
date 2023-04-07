dda2507c3fe18210dc9c99b91d39b9f322506152
Antoniomary <amanso.totti@yahoo.com>
2023-04-07 11:16:28 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,7 +4,8 @@
  * main - a program that multiplies two numbers.
  * @argc: the number of arguments entered to run the program
  * @argv: a pointer to the strings in argc.
- * Return: mul (success), else
+ * Return: 0 (success), else 
+ * 1 (error) if argc is less than 3.
  */
 int main(int argc, char *argv[])
 {
