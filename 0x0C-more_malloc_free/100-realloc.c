82ad7326e438b810fced427f6b79fdd0fb9c9a03
Antoniomary <amanso.totti@yahoo.com>
2023-04-13 18:04:32 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,7 +10,7 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	unsigned int i;
-	char *new_ptr, *temp, *ptr1 = ptr;
+	char *new_ptr, *temp;
 
 	if (new_size == old_size)
 		return (ptr);
@@ -26,26 +26,26 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
-/* allocates new_size to a new memory block */
+
+	/* allocates new_size to a new memory block */
 	new_ptr = malloc(new_size);
-	temp = malloc(old_size);
-	if ((new_ptr == NULL) || (temp == NULL))
+	if (new_ptr == NULL)
 		return (NULL);
-/* copies the data from ptr to new_ptr */
+
+	/* copies the data from ptr to new_ptr */
+	temp = ptr;
 	if (new_size < old_size)
 	{
 		for (i = 0; i < new_size; i++)
-			temp[i] = ptr1[i];
+			new_ptr[i] = temp[i];
 	}
 	else
 	{
 		for (i = 0; i < old_size; i++)
-			temp[i] = ptr1[i];
+			new_ptr[i] = temp[i];
 	}
+
 	free(ptr);
-	for (i = 0; i < old_size; i++)
-		new_ptr[i] = temp[i];
-	free(temp);
 
 	return (new_ptr);
 }
