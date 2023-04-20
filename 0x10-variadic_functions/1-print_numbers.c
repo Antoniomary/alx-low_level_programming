5946d6116366c17fa133822cf7a8cbef7eb745fe
Antoniomary <amanso.totti@yahoo.com>
2023-04-20 04:17:58 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,7 +13,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list args;
 
-	if (separator != NULL)
+	if (separator == NULL)
 		separator = "";
 
 	va_start(args, n);
