cbcf2bedf6f8db6233a1dad309f946c55f4885d6
Antoniomary <amanso.totti@yahoo.com>
2023-05-02 03:18:32 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -23,7 +23,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		*head = (*head)->next;
-
+		free(temp);
 		return (1);
 	}
 
@@ -35,6 +35,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		temp = temp->next;
 	}
 	aux_temp->next = temp->next;
+	free(temp);
 
 	return (1);
 }
