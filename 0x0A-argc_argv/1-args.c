a280f0ecc632487db87a1842d2cd418e33ac73fe
Antoniomary <amanso.totti@yahoo.com>
2023-10-01 23:37:15 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * main - entry point
+ * main - a program that prints the number of arguments passed into it.
  * @argc: the number of command line arguments
  * @argv: a vector of the command line arguments
  *
- * Return: always 0 (success)
+ * Return: 0 (success), 1 (error)
  */
 int main(int argc, char **argv)
 {
