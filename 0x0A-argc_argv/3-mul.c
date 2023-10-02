69b3911418c0752ae72d9ea250ab2ca3d9d64911
Antoniomary <amanso.totti@yahoo.com>
2023-10-01 23:01:51 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: the number of command line arguments
+ * @argv: a vector of the command line arguments
+ *
+ * Return: always 0 (success)
+ */
+int main(int argc, char **argv)
+{
+	int a, b, result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+
+	result = a * b;
+	printf("%d\n", result);
+
+	return (0);
+}
