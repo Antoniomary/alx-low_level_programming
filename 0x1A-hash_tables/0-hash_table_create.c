42b43b0b99163e2386ce99294ffccfb4cf5c93be
Antoniomary <amanso.totti@yahoo.com>
2023-12-22 06:58:17 -0800

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,23 +8,20 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	unsigned long int i;
 	hash_table_t *table = NULL;
-
-	if (size == 0)
-		return (NULL);
 
 	table = (hash_table_t *) malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
 
-	table->array = (hash_node_t **) calloc(size, sizeof(hash_node_t));
+	table->array = (hash_node_t **) malloc(size * sizeof(hash_node_t));
 	if (table->array == NULL)
-	{
-		free(table);
 		return (NULL);
-	}
 
 	table->size = size;
+	for (i = 0; i < table->size; ++i)
+		table->array[i] = NULL;
 
 	return (table);
 }
